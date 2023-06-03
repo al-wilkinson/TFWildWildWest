f762f422fb56f832a12d1c86a90d33a3c42b66f2
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-03 10:39:10 +1000

    Add first main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">=1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+    organization = "WildWildWest"
+    workspaces {
+      name = "MyWorkspace"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terrastuff"
+  location = "Australia East"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "alw54sdqgghh"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+

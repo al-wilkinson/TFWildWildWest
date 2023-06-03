8563c47c0669b3ba10c599cc83e19faa2ea36e4f
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-04 08:10:15 +1000

    Add subnet + NSG

--- main.tf
+++ main.tf
@@ -31,3 +31,16 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
+
+resource "azurerm_subnet" "snet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}

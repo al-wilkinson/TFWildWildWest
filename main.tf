ecaf00824c8fd3c38b5d000452a5e63f543d9f5e
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-03 13:50:38 +1000

    Format main.tf

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name = "alwkvt133515"
+  name                = "alwkvt133515"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  sku_name = "standard"
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "standard"
 
   access_policy = {
     tenant_id = data.azurerm_client_config.current.tenant_id
@@ -42,66 +42,66 @@ resource "azurerm_key_vault" "kv" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "terrastuffVNet"
-  address_space = ["10.0.0.0/16"]
+  name                = "terrastuffVNet"
+  address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_subnet" "snet" {
-  name = "subnet1"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  aaddress_prefixes = ["10.0.1.0/24"]  
+  aaddress_prefixes    = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name = "nsg1"
-  location = azurerm_resource_group.rg.location
+  name                = "nsg1"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_public_ip" "pubip" {
-  name = "terrastuff-ip"
-  location = azurerm_resource_group.rg.location
+  name                = "terrastuff-ip"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Dynamic"
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "vnic" {
-  name = "terrastuff-vm-nic"
-  location = azurerm_resource_group.rg.location
+  name                = "terrastuff-vm-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name = "terrastuff-vm-ip"
-    subnet_id = azurerm_subnet.snet.id
+    name                          = "terrastuff-vm-ip"
+    subnet_id                     = azurerm_subnet.snet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.pubip.id
+    public_ip_address_id          = azurerm_public_ip.pubip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name = "terrastuff-vm"
-  location = azurerm_resource_group.rg.location
+  name                = "terrastuff-vm"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_network_security_group.nsg.name
 
-  size = "Standard_B1s"
+  size           = "Standard_B1s"
   admin_username = "adminuser"
   admin_password = "Howtoprotectthis1?"
 
   network_interface_ids = [azurerm_network_interface.vnic.id]
 
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "20.04-LTS"
-    version = "latest"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
   }
 
   identity {

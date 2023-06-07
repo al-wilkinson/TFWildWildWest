bb378c5a32eb44d92b8e7c9105f51e456769886c
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-07 18:26:01 +1000

    Add data statement and key vault to main.tf

--- main.tf
+++ main.tf
@@ -14,15 +14,45 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
   features {}
   # skip_provider_registration = true
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "terrastuff"
   location = "Australia East"
+}
+
+resource "azurerm_key_vault" "kvt" {
+  name                        = "alwkvt134153"
+  location                    = azurerm_resource_group.kvt.location
+  resource_group_name         = azurerm_resource_group.kvt.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
 
 resource "azurerm_virtual_network" "vnet" {

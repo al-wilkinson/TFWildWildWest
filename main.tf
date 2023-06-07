4420924d6de81c927e9e266858d07bb37fa74157
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-07 18:27:59 +1000

    Fix RG references

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_key_vault" "kvt" {
   name                        = "alwkvt134153"
-  location                    = azurerm_resource_group.kvt.location
-  resource_group_name         = azurerm_resource_group.kvt.name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
